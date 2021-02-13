e61c5395a08aa7c20bb2643fe83a285ff830f360
maximka <maximkv98@gmail.com>
2021-02-13 21:52:01 +0300

    шоб не лмоалось

---
+++ header.js
@@ -0,0 +1,32 @@
+
+
+import React, { useEffect } from 'react';
+import Navigation from './navigation';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Appbar } from 'react-native-paper';
+
+
+
+const Header = () => {
+
+ 
+
+    const _goBack = () => console.log('Went back');
+
+    const _handleSearch = () => console.log('Searching');
+
+    const _handleMore = () => console.log('Shown more');
+
+    return (
+        <Appbar.Header>
+            <Appbar.BackAction onPress={_goBack} />
+            <Appbar.Content title="Title" subtitle="Subtitle" />
+            <Appbar.Action icon="magnify" onPress={_handleSearch} />
+            <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+
+        </Appbar.Header>
+    );
+};
+
+
+export default Header;
